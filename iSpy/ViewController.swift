e6739c646b32bd926e8de0d51902f4e5c77b426a
Brett Davis <brettd1991@yahoo.com>
2014-09-20 10:33:29 -0500

    Initial Commit

---
+++ iSpy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSpy
+//
+//  Created by brett davis on 9/20/14.
+//  Copyright (c) 2014 brett davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
